65744a1af72d4cfc5c13bbd0a24de7ec9996333c
Sun Hong Jae <sunhongjae@gmail.com>
2026-03-26 16:40:38 +0900

    Lvalue && Rvalue

--- Clone/0310.cpp
+++ Clone/0310.cpp
@@ -5,20 +5,22 @@ using std::endl;
 
 // 1. void tmepFunc(int num, int num){} 이랑 void tempFunc(int& num, int& num){}
 // 정확히 컴파일러가 구분하는지 궁금하네.
+using namespace std;
 
-std::pair<int, int> tempFunc(int n1, int n2)
+void distinguishValue(int& a)
 {
-    return {n1, n2};
+    cout << "Lvalue." << endl;
 }
 
-std::pair<const int&, const int&> tempFunc(const int& n1, const int& n2)
+void distinguishValue(int&& a)
 {
-    return {n1, n2};
+    cout << "Rvalue." << endl;
 }
-// 참조자 반환하면 나중에 받은 참조자로 가리켜도 쓰레기값을 가리킴. 
-// n1, n2는 사라지니까 메모리에서.
 
 int main()
 {
-    tempFunc(10, 20);
+    int a = 100;
+    distinguishValue(a);    // 이름 있는 메모리
+    distinguishValue(1000); //임시 객체
+    distinguishValue(std::move(a));
 }
