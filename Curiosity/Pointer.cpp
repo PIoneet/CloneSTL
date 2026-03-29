46cbc15f94ddb7987de5a1d3706c70c188821b5e
Sun Hong Jae <sunhongjae@gmail.com>
2026-03-29 20:54:15 +0900

    Replace int* num to Smart Pointer

--- Curiosity/Pointer.cpp
+++ Curiosity/Pointer.cpp
@@ -17,25 +17,23 @@ public:
         cout << "Copy Constructor valid" << endl;
     }
     
-    Dog(Dog&& other) : num(other.num) { //우값만 받을 수 있음(수정 가능)
+    Dog(Dog&& other) : num(std::move(other.num)) { //우값만 받을 수 있음(수정 가능)
         cout << "Move Constructor valid" << endl;
     } 
 
-    ~Dog(){
-        if(num){
-            cout << "Delete Heap memory" << endl;
-            delete num;
-        }
-        else{
-            cout << "No Heap Memories left" << endl;
-        }
-    }
+    //소멸자가 필요가 없어진다. 스마트 포인터가 알아서 해제해주니까 말이다.
 
 private:
-    int* num;
+    std::unique_ptr<int> num;
 
 friend ostream& operator<<(ostream& os, const Dog& dog){
-    os << *dog.num;
+    if(dog.num){
+        os << *dog.num;
+    }
+    else{
+        os << "Invalid Access" << endl;
+    }
+    
     return os;
 }
 
@@ -43,24 +41,15 @@ friend ostream& operator<<(ostream& os, const Dog& dog){
 
 int main()
 {
-    const Dog d1{1102};
+    Dog d1{1102};
     Dog d2 = std::move(d1);
     cout << d2 << endl;
-    // const d1을 줬기에 원본 수정이 안되서 복사 생성자가 호출됨.
-    // 아예 별개의 new int{*other.num} 메모리를 생성해서 Double Free 아님. 
+    cout << d1 << endl; 
 
 }
 
-
-// 오케이 스마트 포인터 정리를 해보겠다. 
-// 일단 스마트 포인터는 무조건 초기화할떄 우값 Rvalue를 받는다. 
-// 즉 이름이 없는 메모리 주소값을 받는다는 애기다. 그 이유는 소유권을 독점하기 위해
-// 만약 좌값을 받는 다면 p2 = p1; 처럼 복사가 가능해야 하기 떄문이다. 
-
-// 근데 Dog&& other로 우값을 받으면 그 시점에서 other는 이름이 생겼기에
-// 좌값이 된다. -> 따라서 other.num 같이 멤버 변수에 접근할 수 있는 것이다.
-// 또한, && 또한 원본 데이터의 그저 별칭이기 떄문이다.
-
+// std::unique_ptr<int> num; 이 되면 num을 other.num으로 초기화 할 수가 없다.
+// other.num은 Lvalue니까 말이다.
 
 
 
