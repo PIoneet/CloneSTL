8976f519f14b1a39a41768b213351342abde19a3
Sun Hong Jae <sunhongjae@gmail.com>
2026-04-15 17:10:07 +0900

    reverse new_file, istream_iterator 입력 반복자 사용해보기

--- Curiosity/vector_sort.cpp
+++ Curiosity/vector_sort.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <iterator>
+#include <string>
+
 
 using std::vector;
 using std::ifstream;
 using std::cout;
 using std::endl;
+using std::string;
+using std::cin;
+using std::ofstream;
+
+
 
 int main()
 {
-    vector<char> k;
-    k.reserve(10'0000);
-    cout << "k의 힙 주소: " << (void*)k.data() << endl;
-    
-    
-    ifstream in{"vector_sort.cpp"};
-    char c;
+
+    ifstream in{"vector_sort.cpp", std::ios::binary};
+
+    std::istream_iterator<char> start_file{in};
+    std::istream_iterator<char> end_file{};
+
     in >> std::noskipws;
-    while(in >> c){  // 파일을 다 읽으면 탈출한다.
-        cout << "k의 용량: " << k.capacity() << endl;
-        cout << "k의 힙 주소: " << (void*)k.data() << endl;
-        k.push_back(c);
+    vector<char> storeFile(start_file, end_file);
+
+    cout << static_cast<unsigned long long>(in.tellg()) << endl;
+
+    // 벡터에는 이 입력 반복자 2개 받는 생성자가 존재한다. 
+
+    for(auto i = storeFile.rbegin(); i != storeFile.rend(); ++i){
+        cout << *i;
     }
 
+    ofstream out{"new_file.txt" ,std::ios::binary};
+
+    //out.write(reinterpret_cast<char*>(storeFile.data()), sizeof(storeFile));
+    // 이렇게 전달하면 스택 메모리 24바이트만 반환한다. 
+    
+    std::reverse(storeFile.begin(), storeFile.end());
+    out.write(reinterpret_cast<char*>(storeFile.data()), storeFile.size() * sizeof(char));
     
     /*
-    for(char c:k)
+    for(char c : storeFile){
         cout << c;
-    cout << endl;
+    }
     */
+
 }
