7cc84455e5f1497daa438f2be55cefc8142376d2
Sun Hong Jae <sunhongjae@gmail.com>
2026-04-19 16:27:25 +0900

    람다가 전역함수보다 빠름. function<> 함수 포인터보다 auto로 받는게 더 빠름.  function은 저장 교체 전달이 가능.

--- Curiosity/0310/learn_template.cpp
+++ Curiosity/0310/learn_template.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <functional>
+#include <vector>
 
 using namespace std;
 
@@ -8,26 +10,64 @@ void change(T& a, T& b)
     T temp{a};
     a= b;
     b = temp;
+
+    cout << "Template change " << endl;
 }
 
 
-template<class T>
-void change(T&& a, T&& b)
+void change(int& a, int& b)
 {
-    T temp = a;
+    int temp = a;
     a = b;
     b = temp;
-    cout << "RValue" << endl;
-    cout << a << " " << b << endl;
+
+    cout << "Global change " << endl;
+
+    //타입 정확히 일치하는게 호출 우선순위 높음.
+    // 템플릿이 컴파일할떄 좀 더 걸림.
 }
+
+
 
 
 int main()
 {
+    vector<function<void(int&)>> funcs;
+    funcs.reserve(10'0000);
+    for(int i=0; i<5; ++i){
+        funcs.push_back([](int& i){
+            
+            cout << i << endl;
+        });
+        cout << "size: " << funcs.size() << endl;
+        funcs[i](i);
+        // 캡처에 넣었고 아까는 인자가 필요없었음.
+    }
+
     int a{1}, b{2};
-    //change(a, b);
+    //function<void(int&, int&)> change; 
 
-    change(2, 5);
+    /*
+    void (*change)(int&, int&) = [](int& a, int& b){
+        int temp = a;
+        a = b;
+        b = temp;
+        cout << "Local Lambda change" << endl;
+    };
+    */
 
-    //cout << a << " " << b;
+    auto change = [&a, &b](){
+        int temp = a;
+        a = b;
+        b = temp;
+        cout << "Captur Local Lambda change" << endl;
+    };
+
+    change(); //캡처는 인자로 안받으니까 이렇게 쓴다.
+
+    cout << a << " " << b;
 }
+
+
+// 함수 포인터는 멤버 변수를 담을 수 없다. 오로지 함수의 메모리 주소만 저장하기 때문.
+// []안에 들어가는걸 캡처라고 부르는데 외부 변수를  람다 내부에서 사용하는 경우를 애기함.
